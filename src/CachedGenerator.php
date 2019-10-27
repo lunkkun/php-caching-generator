fa012d62ea119e0bff8f9525c5a52fdfc1e92d4f
Luuk Linders <luuk.linders@movements.nl>
2019-10-27 22:18:55 +0100

    Added code and tests

---
+++ src/CachedGenerator.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Lunkkun\CachedGenerator;
+
+use Generator;
+use Iterator;
+
+class CachedGenerator implements Iterator
+{
+    /** @var Iterator */
+    protected $generator;
+    /** @var array */
+    protected $cache = [];
+
+    public function __construct(Generator $generator)
+    {
+        $this->generator = $generator;
+        $this->addCurrentToCache();
+    }
+
+    public function current()
+    {
+        return current($this->cache);
+    }
+
+    public function next(): void
+    {
+        if ($this->generator->key() === key($this->cache)) {
+            $this->generator->next();
+            $this->addCurrentToCache();
+        }
+        next($this->cache);
+    }
+
+    public function key(): int
+    {
+        return key($this->cache);
+    }
+
+    public function valid(): bool
+    {
+        return key($this->cache) !== null;
+    }
+
+    public function rewind(): void
+    {
+        reset($this->cache);
+    }
+
+    protected function addCurrentToCache(): void
+    {
+        if ($this->generator->valid()) {
+            $this->cache[] = $this->generator->current();
+        }
+    }
+}
---
+++ tests/CachedGeneratorTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Lunkkun\CachedGenerator\Tests;
+
+use Lunkkun\CachedGenerator\CachedGenerator;
+use PHPUnit\Framework\TestCase;
+
+class CachedGeneratorTest extends TestCase
+{
+    public function testGenerates() {
+        $generator = function () {
+            foreach (range(0, 2) as $value) {
+                yield $value;
+            }
+        };
+        $cachedGenerator = new CachedGenerator($generator());
+
+        $results = iterator_to_array($cachedGenerator);
+        $this->assertEquals(range(0, 2), $results);
+    }
+
+    public function testWorksWithEmptyGenerator() {
+        $generator = function () {
+            if (false) yield 0;
+        };
+        $cachedGenerator = new CachedGenerator($generator());
+
+        $results = iterator_to_array($cachedGenerator);
+        $this->assertEquals([], $results);
+    }
+
+    public function testWorksTwice() {
+        $generator = function () {
+            foreach (range(0, 2) as $value) {
+                yield $value;
+            }
+        };
+        $cachedGenerator = new CachedGenerator($generator());
+
+        iterator_to_array($cachedGenerator);
+
+        $results = iterator_to_array($cachedGenerator);
+        $this->assertEquals(range(0, 2), $results);
+    }
+}
